b0109adcda7b3f46e0a1dc468a779a93b045a51e
nguyenthanhthao02 <91305311+nguyenthanhthao02@users.noreply.github.com>
2022-11-05 13:32:17 +0700

    Add content to home
    Add content to the home page.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -32,7 +32,11 @@ const Home = (props)=>{
                             <h3>Submit your contribution</h3>
                         </div>
                         <div className="item-main">
-                            
+                            <p>Sumbit your document, research paper, image, data set and more into VinUni's digital respository.</p>
+                        </div>
+                        <div>
+                            <button className="submit-button">Submit your work</button>
+                            <p className='submit-text'>How to submit?</p>
                         </div>
                     </div>
 
@@ -43,7 +47,16 @@ const Home = (props)=>{
                             <h3>Browse by</h3>
                         </div>
                         <div className="item-main">
-                            
+                            <ul> 
+                                <li>Communities & Collections</li>
+                                <li>Issue Dates</li>
+                                <li>Tittles</li>
+                                <li>Authors</li>
+                                <li>Subjects</li>
+                            </ul>
+                        </div>
+                        <div className = "collections">
+                            <span>View all collections</span>
                         </div>
                     </div>
 
@@ -52,7 +65,16 @@ const Home = (props)=>{
                             <h3>Browse Collections</h3>
                         </div>
                         <div className="item-main">
-                            
+                            <ul> 
+                                <li>College of Business & Management</li>
+                                <li>College of Engineering & Computer Science</li>
+                                <li>College of Health Science</li>
+                                <li>College of Arts & Science</li>
+                                <li>Smart Health Center</li>
+                            </ul>
+                        </div>
+                        <div className = "collections">
+                            <span>View all collections</span>
                         </div>
                     </div>
 
