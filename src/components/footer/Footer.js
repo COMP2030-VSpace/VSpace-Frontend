62a8987be745d943a04b864e13b3c2258a4c270b
tunghng <lehoangtung081003@gmail.com>
2022-11-06 22:45:32 +0700

    footer commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,80 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {useEffect, createRef, useContext, useState} from 'react';
-import './footer.scss';
+import { useEffect, createRef, useContext, useState } from "react";
+import "./footer.scss";
+import logo from "../../assets/footer/logo-vinuni-white.png.png";
+import fb from "../../assets/footer/facebook-app-symbol 1.png";
+import mail from "../../assets/footer/Mail.png";
+import yt from "../../assets/footer/Vector.png";
 
 // import assets
 // import sample_banner from "../../assets/sample/banner/banner.png";
 
 // import components;
 
-const Footer = (props)=>{
-    return(
-        <div className = 'footer'>
-            
+const Footer = (props) => {
+  return (
+    <footer className="footer">
+      <div className="footer--top">
+        <div className="footer--image">
+          <img src={logo} />
         </div>
-    );
-}
+
+        <div className="footer--content">
+          <div className="wrapper">
+            <h5>VinSpace@Vinuni</h5>
+            <ul>
+              <li>Vinhomes Ocean Park</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer--content">
+          <div className="wrapper">
+            <h5>VinSpace Services</h5>
+            <ul>
+              <li>Browse Collection</li>
+              <li>Search Documents</li>
+              <li>Submit Articles</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="footer--content">
+          <div className="wrapper">
+            <h5>About us</h5>
+            <ul>
+              <li>VinSpace Policy</li>
+              <li>Supporting DSpace</li>
+              <li>VinSpace Employment</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer--bottom">
+        <div>
+          <p className="copyright">
+            Copyright <span>&copy;</span> 2022 VinUni. All Right Reserved.
+            Privacy Policy
+          </p>
+        </div>
+
+        <div className="social-links">
+          <ul >
+            <li>
+              <img src={fb} />
+            </li>
+            <li>
+              <img src={yt} />
+            </li>
+            <li>
+              <img src={mail} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
