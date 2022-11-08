0f55889308cf1613f621f44ce1c48eceaed43e74
ThaiHungN <hungnguyenhsgs@gmail.com>
2022-11-08 15:01:31 +0700

    update a lot

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,69 +12,75 @@ import yt from "../../assets/footer/Vector.png";
 // import components;
 
 const Footer = (props) => {
-  return (
-    <footer className="footer">
-      <div className="footer--top">
-        <div className="footer--image">
-          <img src={logo} />
-        </div>
+    return (
+        <div className="footer">
 
-        <div className="footer--content">
-          <div className="wrapper">
-            <h5>VinSpace@Vinuni</h5>
-            <ul>
-              <li>Vinhomes Ocean Park</li>
-              <li>Contact us</li>
-            </ul>
-          </div>
-        </div>
+            <div className="padding"></div>
 
-        <div className="footer--content">
-          <div className="wrapper">
-            <h5>VinSpace Services</h5>
-            <ul>
-              <li>Browse Collection</li>
-              <li>Search Documents</li>
-              <li>Submit Articles</li>
-            </ul>
-          </div>
-        </div>
+            <div className="outer-wrapper">
+                <div className="footer--top">
+                    <div className="footer--image">
+                        <img src={logo} />
+                    </div>
 
-        <div className="footer--content">
-          <div className="wrapper">
-            <h5>About us</h5>
-            <ul>
-              <li>VinSpace Policy</li>
-              <li>Supporting DSpace</li>
-              <li>VinSpace Employment</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div className="footer--bottom">
-        <div>
-          <p className="copyright">
-            Copyright <span>&copy;</span> 2022 VinUni. All Right Reserved.
-            Privacy Policy
-          </p>
-        </div>
+                    <div className="footer--content">
+                        <div className="wrapper">
+                            <h5>VinSpace@Vinuni</h5>
+                            <ul>
+                                <li>Vinhomes Ocean Park</li>
+                                <li>Contact us</li>
+                            </ul>
+                        </div>
+                    </div>
 
-        <div className="social-links">
-          <ul>
-            <li>
-              <img src={fb} width="49" />
-            </li>
-            <li>
-              <img src={yt} width="55" />
-            </li>
-            <li>
-              <img src={mail} width="50" />
-            </li>
-          </ul>
+                    <div className="footer--content">
+                        <div className="wrapper">
+                            <h5>VinSpace Services</h5>
+                            <ul>
+                                <li>Browse Collection</li>
+                                <li>Search Documents</li>
+                                <li>Submit Articles</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="footer--content">
+                        <div className="wrapper">
+                            <h5>About us</h5>
+                            <ul>
+                                <li>VinSpace Policy</li>
+                                <li>Supporting DSpace</li>
+                                <li>VinSpace Employment</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer--bottom">
+                    <div>
+                        <p className="copyright">
+                            Copyright <span>&copy;</span> 2022 VinUni. All Right Reserved.
+                            Privacy Policy
+                        </p>
+                    </div>
+
+                    <div className="social-links">
+                        <ul>
+                            <li>
+                                <img src={fb} width="49" />
+                            </li>
+                            <li>
+                                <img src={yt} width="55" />
+                            </li>
+                            <li>
+                                <img src={mail} width="50" />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </footer>
-  );
+    );
 };
 
 export default Footer;
