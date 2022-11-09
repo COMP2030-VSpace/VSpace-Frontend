1d9cdfcb0f91680003b41d9b51538bf399d4fa77
tunghng <lehoangtung081003@gmail.com>
2022-11-09 15:02:41 +0700

    about top commit

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,35 +1,58 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {useEffect, createRef, useContext, useState} from 'react';
-import './about.scss';
-
+import { useEffect, createRef, useContext, useState } from "react";
+import "./about.scss";
 
 // import components
 import Navbar from "../../components/navbar/Navbar";
-import Banner from '../../components/banner/Banner';
+import Banner from "../../components/banner/Banner";
 import Footer from "../../components/footer/Footer";
 
+import arrow from "../../assets/about/arrow.png";
 
-const About = (props)=>{
-    return(
-        <div className = 'about'>
+const About = (props) => {
+    return (
+        <div className="about">
             <Navbar></Navbar>
             <Banner></Banner>
 
             <div className="main">
-                <div className='gen-intro'>
-                    {/* Tung works here */}
+                <div className="gen-intro">
+                    <div className="content">
+                        <h2>LIBRARY</h2>
+                        <span>
+                            VinUni Library seeks to advance the intellectual
+                            life of the University community through resources
+                            and services that support faculty research as well
+                            as students’ successful learning outcomes and
+                            personal development.
+                        </span>
+
+                        <br />
+                        <br />
+
+                        <span>
+                            The library offers 4 main types of spaces to support
+                            learning, teaching, research, innovation, and
+                            entrepreneurial activities.
+                        </span>
+                    </div>
+
+                    <div className="button-div">
+                        <button className="button"  >
+                            <div className="button-content">
+                                <span>SEARCH THE LIBRARY</span>
+                                <img src={arrow} style={{ width: "20px" }} />
+                            </div>
+                        </button>
+                    </div>
                 </div>
 
-
-
-                <div className='two-col'>
-                    {/* Thao works here */}
-                </div>
+                <div className="two-col">{/* Thao works here */}</div>
             </div>
 
             <Footer></Footer>
         </div>
     );
-}
+};
 
 export default About;
