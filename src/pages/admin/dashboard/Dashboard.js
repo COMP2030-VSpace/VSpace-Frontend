ba28a4c682465b0aa979b4f2a8579d0b7218746b
tunghng <lehoangtung081003@gmail.com>
2022-11-18 11:18:05 +0700

    absolute commit

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -1,27 +1,57 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {useEffect, createRef, useContext, useState} from 'react';
-import './dashboard.scss';
-
+import { useEffect, createRef, useContext, useState } from "react";
+import "./dashboard.scss";
+import { Container } from "reactstrap";
 
 // import components
-import Navbar from '../../../components/navbar/Navbar';
-import Footer from '../../../components/footer/Footer';
-import SideMenu from '../../../components/sideMenu/SideMenu';
-import Banner from '../../../components/banner/Banner';
+import Navbar from "../../../components/navbar/Navbar";
+import Footer from "../../../components/footer/Footer";
+import SideMenu from "../../../components/sideMenu/SideMenu";
+import Banner from "../../../components/banner/Banner";
 
-const Dashboard = (props)=>{
-    return(
-        <div className = 'dashboard'>
+import search from "../../../assets/admin/search-admin.png";
+
+const Dashboard = (props) => {
+    return (
+        <div className="dashboard">
             <Navbar></Navbar>
-            
-            <div className='main'>
+            <div className="main">
                 <SideMenu></SideMenu>
-
-
                 {/* Tung starts here */}
 
-                <div className='main-right'>
+                <div className="main-right">
                     <Banner></Banner>
+                </div>
+
+                <div className="search">
+                    
+                        <input
+                            type="text"
+                            placeholder="Search the repository"
+                            className="searchbar"
+                        />
+                    <div className="button">
+                        <img
+                            src={search}
+                            alt=""
+                            className="searchimage"
+                            style={{ width: "37px", height: "37px" }}
+                        />
+                        <span style={{ color: "white" }}>Search</span>
+                    </div>
+                </div>
+                <div className="text">
+                    <h2>Community in DSpace</h2>
+                    <h3>Select a community to browse its collections.</h3>
+                    <span>Now showing 1 - 5 of 547</span>
+                    <ul>
+                        <li>000_Vectino Natalia</li>
+                        <li>000AgusGuarez</li>
+                        <li>000AliciaAM</li>
+                        <li>000_Vectino Natalia</li>
+                        <li>000AgusGuarez</li>
+                        <li>000AliciaAM</li>
+                    </ul>
                 </div>
 
                 {/* Tung ends here */}
@@ -30,6 +60,6 @@ const Dashboard = (props)=>{
             <Footer></Footer>
         </div>
     );
-}
+};
 
 export default Dashboard;
