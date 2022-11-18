1beb8cbd241b15183c237375eb9f056f6177a421
ThaiHungN <hungnguyenhsgs@gmail.com>
2022-11-18 23:35:45 +0700

    fix admin outline

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, createRef, useContext, useState } from "react";
 import "./dashboard.scss";
-import { Container } from "reactstrap";
+// import { Container } from "reactstrap";
 
 // import components
 import Navbar from "../../../components/navbar/Navbar";
@@ -15,46 +15,133 @@ const Dashboard = (props) => {
     return (
         <div className="dashboard">
             <Navbar></Navbar>
+
             <div className="main">
+                <div className="sidemenu-bg"></div>
                 <SideMenu></SideMenu>
-                {/* Tung starts here */}
 
                 <div className="main-right">
+                    {/* Tung starts here */}
                     <Banner></Banner>
-                </div>
 
-                <div className="search">
-                    
-                        <input
-                            type="text"
-                            placeholder="Search the repository"
-                            className="searchbar"
-                        />
-                    <div className="button">
-                        <img
-                            src={search}
-                            alt=""
-                            className="searchimage"
-                            style={{ width: "37px", height: "37px" }}
-                        />
-                        <span style={{ color: "white" }}>Search</span>
+                    <div className="search">
+                        
+                            <input
+                                type="text"
+                                placeholder="Search the repository"
+                                className="searchbar"
+                            />
+                        <div className="button">
+                            <img
+                                src={search}
+                                alt=""
+                                className="searchimage"
+                                style={{ width: "37px", height: "37px" }}
+                            />
+                            <span style={{ color: "white" }}>Search</span>
+                        </div>
                     </div>
-                </div>
-                <div className="text">
-                    <h2>Community in DSpace</h2>
-                    <h3>Select a community to browse its collections.</h3>
-                    <span>Now showing 1 - 5 of 547</span>
-                    <ul>
-                        <li>000_Vectino Natalia</li>
-                        <li>000AgusGuarez</li>
-                        <li>000AliciaAM</li>
-                        <li>000_Vectino Natalia</li>
-                        <li>000AgusGuarez</li>
-                        <li>000AliciaAM</li>
-                    </ul>
-                </div>
+                    
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
 
-                {/* Tung ends here */}
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    <div className="text">
+                        <h2>Community in DSpace</h2>
+                        <h3>Select a community to browse its collections.</h3>
+                        <span>Now showing 1 - 5 of 547</span>
+                        <ul>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                            <li>000_Vectino Natalia</li>
+                            <li>000AgusGuarez</li>
+                            <li>000AliciaAM</li>
+                        </ul>
+                    </div>
+
+                    {/* Tung ends here */}
+                </div>
             </div>
 
             <Footer></Footer>
