152a1011c18062175b900233799b2c78c851518e
ThaiHungN <hungnguyenhsgs@gmail.com>
2022-11-20 10:50:26 +0700

    fix navbar in admin

--- src/pages/admin/dashboard/Dashboard.js
+++ src/pages/admin/dashboard/Dashboard.js
@@ -17,60 +17,66 @@ const Dashboard = (props) => {
         <div className="dashboard">
             <Navbar></Navbar>
 
-            <Banner className="banner"></Banner>
+            {/* <Banner></Banner> */}
+
             <div className="main">
                 {/* <div className="sidemenu-bg"></div> */}
                 <SideMenu></SideMenu>
 
                 <div className="main-right">
                     {/* Tung starts here */}
+                    
+                    <div className="customized-banner">
+                        <Banner></Banner>
+                    </div>
 
-                    <div className="search">
-                        <input
-                            type="text"
-                            placeholder="Search the repository"
-                            className="searchbar"
-                        />
-                        <div className="button">
-                            <img
-                                src={search}
-                                alt=""
-                                className="searchimage"
-                                style={{ width: "37px", height: "37px" }}
+                    <div className="wrapper">                        
+                        <div className="search">
+                            <input
+                                type="text"
+                                placeholder="Search the repository"
+                                className="searchbar"
                             />
-                            <span style={{ color: "white" }}>Search</span>
+                            <div className="button">
+                                <img
+                                    src={search}
+                                    alt=""
+                                    className="searchimage"
+                                    style={{ width: "37px", height: "37px" }}
+                                />
+                                <span style={{ color: "white" }}>Search</span>
+                            </div>
+                        </div>
+
+                        <div className="text">
+                            <h2 className="text__header">Community in DSpace</h2>
+                            <h3 className="text__secondheader">Select a community to browse its collections.</h3>
+                            <p className="header__ul">Now showing 1 - 5 of 547</p>
+                            <ul>
+                                <li>000_Vectino Natalia</li>
+                                <li>000AgusGuarez</li>
+                                <li>000AliciaAM</li>
+                                <li>000_Vectino Natalia</li>
+                                <li>000AgusGuarez</li>
+                                <li>000AliciaAM</li>
+                            </ul>
+                        </div>
+
+                        <div className="blocklist">
+                            <div className="block" style={{ borderTopLeftRadius: "10px", borderBottomLeftRadius: "10px" }}><img src={arrow} alt="" /></div>
+                            <div className="block">1</div>
+                            <div className="block">2</div>
+                            <div className="block">3</div>
+                            <div className="block">4</div>
+                            <div className="block">5</div>
+                            <div className="block">6</div>
+                            <div className="block">7</div>
+                            <div className="block">8</div>
+                            <div className="block">9</div>
+                            <div className="block">...</div>
+                            <div className="block" style={{ borderTopRightRadius: "10px", borderBottomRightRadius: "10px" }}>20</div>
                         </div>
                     </div>
-
-                    <div className="text">
-                        <h2 className="text__header">Community in DSpace</h2>
-                        <h3 className="text__secondheader">Select a community to browse its collections.</h3>
-                        <p className="header__ul">Now showing 1 - 5 of 547</p>
-                        <ul>
-                            <li>000_Vectino Natalia</li>
-                            <li>000AgusGuarez</li>
-                            <li>000AliciaAM</li>
-                            <li>000_Vectino Natalia</li>
-                            <li>000AgusGuarez</li>
-                            <li>000AliciaAM</li>
-                        </ul>
-                    </div>
-
-                    <div className="blocklist">
-                        <div className="block" style={{ borderTopLeftRadius: "10px", borderBottomLeftRadius: "10px" }}><img src={arrow} alt="" /></div>
-                        <div className="block">1</div>
-                        <div className="block">2</div>
-                        <div className="block">3</div>
-                        <div className="block">4</div>
-                        <div className="block">5</div>
-                        <div className="block">6</div>
-                        <div className="block">7</div>
-                        <div className="block">8</div>
-                        <div className="block">9</div>
-                        <div className="block">...</div>
-                        <div className="block" style={{ borderTopRightRadius: "10px", borderBottomRightRadius: "10px" }}>20</div>
-                    </div>
-
                     {/* Tung ends here */}
 
                     {/* Thao starts here */}
