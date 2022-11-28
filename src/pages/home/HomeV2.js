053d57a91a5c3f7aa830dd35fc7e28fb3262c6db
hungnt16 <hungnt@cystack.net>
2022-11-28 09:15:22 +0700

    Update HomeV2

--- src/pages/home/HomeV2.js
+++ src/pages/home/HomeV2.js
@@ -14,6 +14,7 @@ const HomeV2 = (props) => {
         <div className="home">
             <div className="pageContentWrapper">
                 <Navbar></Navbar>
+                <Banner></Banner>
 
                 <div className="main">
                     <div className="left">
@@ -67,12 +68,12 @@ const HomeV2 = (props) => {
                             <HomeSearch></HomeSearch>
                         </div>
 
-                        <div className="browse item">
+                        <div className="browse item mb2">
                             <div className="text mb1">BROWSE</div> 
 
                             <div className="vinspace-list">
                                 <div className="heading">
-                                    ALL OF VINSPACE
+                                    ALL OF VinSpace
                                 </div>
 
                                 <div className="browse-list">
@@ -82,6 +83,58 @@ const HomeV2 = (props) => {
                                         <li>Authors</li>
                                         <li>Titles</li>
                                         <li>Subjects</li> 
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="browse item">
+                            <div className="text mb1">DISCOVER</div> 
+
+                            <div className="vinspace-list mb1">
+                                <div className="heading">
+                                    Author
+                                </div>
+
+                                <div className="browse-list">
+                                    <ul>
+                                        <li>Anderson, Terry (121)</li>
+                                        <li>McGreal, Rory (95)</li>
+                                        <li>Gismondy, Mike (82)</li>
+                                        <li>Temple Norman, J (22)</li>
+                                        <li>Holmberg, Robert G (35)</li> 
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="vinspace-list mb1">
+                                <div className="heading">
+                                    Subject
+                                </div>
+
+                                <div className="browse-list">
+                                    <ul>
+                                        <li>Holmberg, Robert G (35)</li>
+                                        <li>Distance Education</li>
+                                        <li>Mobile Learning</li>
+                                        <li>Social Enterprise</li>
+                                        <li>Research</li> 
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="vinspace-list">
+                                <div className="heading">
+                                    Date Issued
+                                </div>
+
+                                <div className="browse-list">
+                                    <ul>
+                                        <li>2020 - 2022 (33)</li>
+                                        <li>2010 - 2019 (19)</li>
+                                        <li>2000 - 2009 (22)</li>
+                                        <li>1990 - 1999 (14)</li>
+                                        <li>1980 - 1989 (12)</li> 
                                     </ul>
                                 </div>
                             </div>
