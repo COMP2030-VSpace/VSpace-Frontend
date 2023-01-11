cec0c91b6f674c63b4242a1761f83b7a13e13364
nguyenthanhthao02 <91305311+nguyenthanhthao02@users.noreply.github.com>
2023-01-11 10:07:08 +0700

    add to search

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -12,14 +12,54 @@ import Item from "../../components/item/Item";
 import { moveTo } from "../../utils/helperFunctions";
 
 // import assets
-
+import thumb from "../../assets/admin/thumbnail.png";
 import arrow from '../../assets/search/arrow.png';
 
 // import context
 import { CommunityContext } from "../../contexts/CommunityContext";
+import { ItemContext } from "../../contexts/ItemContext";
 
 const Search = (props) => {
+    const { getCommunities } = useContext(CommunityContext);
+    const { getRecentItemsFromSite } = useContext(ItemContext);
+    
+    const [communities, setCommunities] = useState([])
+    const [curPage, setCurPage] = useState(1);
+    const [totalElements, setTotalElements] = useState(1);
+    const [totalPage, setTotalPage] = useState(0);
+    const itemsPerPage = 5;
+    const [recentItems, setRecentItems] = useState([]);
 
+    useEffect(() => {
+        const loadCommunities = async () => {
+            const page = curPage - 1;
+            const response = await getCommunities(page, itemsPerPage);
+            // console.log(response);
+            const pageInfo = response.data.page
+            setTotalPage(pageInfo.totalPages)
+            setTotalElements(pageInfo.totalElements);
+
+            const data = response.data["_embedded"].communities
+
+            // console.log(data);
+
+            setCommunities(data);
+            
+        }
+
+        const loadRecentItems = async () => {
+            const response = await getRecentItemsFromSite();
+
+            // console.log(response);
+
+            const data = response.data["_embedded"].searchResult["_embedded"].objects;
+
+            setRecentItems(data);
+        }
+
+        loadCommunities();
+        loadRecentItems();
+    }, [curPage])
 
     return (
         <div className="search">
@@ -34,9 +74,20 @@ const Search = (props) => {
                         </div>
 
                         <div className="search-bar">
-                            <div className="selection-bar">
+                            {/* <div className="selection-bar">
                                 All of VinSpace
                                 <img src = {arrow} alt = "arrow"/>
+                            </div> */}
+
+                            <div className="selection-bar">
+                                <select>
+                                    <option>All of VinSpace</option>
+                                    <option>Communities & Collections</option>
+                                    <option>By Issue Date</option>
+                                    <option>Authors</option>
+                                    <option>Titles</option>
+                                    <option>Subjects</option>
+                                </select>
                             </div>
 
                             <div className='search-input'>
@@ -52,9 +103,55 @@ const Search = (props) => {
                                 Show advanced filters
                         </div>
 
-                        <div className="result-txt">
-                            Search produced no results.
+                        <div className="header-result">
+                            <h2>Search Results</h2>
                         </div>
+
+                        <div className="result-txt">
+                            Now showing 1 - 10 of 8744
+                        </div>
+
+                        <div className="item">
+                                {recentItems.map((item) => {
+                                    const data = item["_embedded"].indexableObject;
+                                    return <div className="item-content">
+                                                <div className="thumb">
+                                                    <img src= {thumb} alt="Thumbnail"/>
+                                                </div>
+
+                                                <div className="thumb-info">
+                                                    <div className="tag">
+                                                        <span className="tag1">datafile.listelement.badge</span>
+                                                        <span className="tag2">Open Access</span>
+                                                    </div>
+
+                                                    <div className="item-header">
+                                                        <h3>{data.metadata["dc.title"][0].value}</h3>
+                                                    </div>
+
+                                                    <div className="item-sub">
+                                                        <h4>
+                                                            (VinSpace, 2022-11-15) {data.metadata["dc.contributor.author"].map((author, idx) => {
+                                                                return <>{author.value}{idx === data.metadata["dc.contributor.author"].length - 1 ? " " : ", "}</>
+                                                            })}
+                                                        </h4>
+                                                    </div>
+
+                                                    <div className="item-main">
+                                                        <div className="content">
+                                                            {data.metadata["dc.description.abstract"][0].value}
+                                                        </div>
+                                                    </div>
+
+                                                    <label for="text-button" className="show-button">
+                                                        <img src={arrow}/>
+                                                        <p>Show more</p>
+                                                    </label>
+                                                </div>
+                                            </div> 
+                                })}
+                        </div>
+
                     </div>
 
                     <div className="right desktop">
